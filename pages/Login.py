9b62ed94c8997c7bc94f915c2f32b5209df09435
Horizon489732 <tranminhhien27102003@gmail.com>
2025-03-17 12:36:41 -0400

    Worked on the Layout for the Employee Dashboard
    Still missing some logics

--- pages/Login.py
+++ pages/Login.py
@@ -48,6 +48,6 @@ class Login(tk.Frame):
 
         if login_functions.check_credentials(username, password, location):
             print(f"Login successful for {username}")
-            self.controller.show_dashboard()
+            self.controller.show_dashboardEmp()
         else:
             print("Invalid credentials, please try again.")
